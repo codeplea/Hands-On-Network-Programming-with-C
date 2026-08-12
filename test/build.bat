744a74e6af71c9990a08c554d9b6db5d8c7f8ba6
Lewis Van Winkle <LV@codeplea.com>
2026-08-12 06:33:59 -0500

    Make test scripts report compile failures

--- test/build.bat
+++ test/build.bat
@@ -1,9 +1,12 @@
+set BUILD_FAILED=0
 copy test\id.c test\id.cpp
 echo "%CC%"
 %CC% test/id.c -o id.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 id.exe
 del id.exe
 %CC% test/id.cpp -o id.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 id.exe
 del id.exe
 del test\id.cpp
@@ -13,9 +16,11 @@ cd chap01
 copy win_init.c win_init.cpp
 echo "%CC%"
 %CC% win_init.c -o win_init.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 win_init.exe
 del win_init.exe
 %CC% win_init.cpp -o win_init.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 win_init.exe
 del win_init.exe
 del win_init.cpp
@@ -26,9 +31,11 @@ cd chap01
 copy win_list.c win_list.cpp
 echo "%CC%"
 %CC% win_list.c -o win_list.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 win_list.exe
 del win_list.exe
 %CC% win_list.cpp -o win_list.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 win_list.exe
 del win_list.exe
 del win_list.cpp
@@ -40,9 +47,11 @@ cd chap02
 copy sock_init.c sock_init.cpp
 echo "%CC%"
 %CC% sock_init.c -o sock_init.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 sock_init.exe
 del sock_init.exe
 %CC% sock_init.cpp -o sock_init.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 sock_init.exe
 del sock_init.exe
 del sock_init.cpp
@@ -53,9 +62,11 @@ cd chap02
 copy time_console.c time_console.cpp
 echo "%CC%"
 %CC% time_console.c -o time_console.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 time_console.exe
 del time_console.exe
 %CC% time_console.cpp -o time_console.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 time_console.exe
 del time_console.exe
 del time_console.cpp
@@ -66,9 +77,11 @@ cd chap02
 copy time_server.c time_server.cpp
 echo "%CC%"
 %CC% time_server.c -o time_server.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM time_server.exe
 del time_server.exe
 %CC% time_server.cpp -o time_server.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM time_server.exe
 del time_server.exe
 del time_server.cpp
@@ -79,9 +92,11 @@ cd chap02
 copy time_server_ipv6.c time_server_ipv6.cpp
 echo "%CC%"
 %CC% time_server_ipv6.c -o time_server_ipv6.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM time_server_ipv6.exe
 del time_server_ipv6.exe
 %CC% time_server_ipv6.cpp -o time_server_ipv6.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM time_server_ipv6.exe
 del time_server_ipv6.exe
 del time_server_ipv6.cpp
@@ -92,9 +107,11 @@ cd chap02
 copy time_server_dual.c time_server_dual.cpp
 echo "%CC%"
 %CC% time_server_dual.c -o time_server_dual.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM time_server_dual.exe
 del time_server_dual.exe
 %CC% time_server_dual.cpp -o time_server_dual.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM time_server_dual.exe
 del time_server_dual.exe
 del time_server_dual.cpp
@@ -105,9 +122,11 @@ cd chap03
 copy tcp_client.c tcp_client.cpp
 echo "%CC%"
 %CC% tcp_client.c -o tcp_client.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 tcp_client.exe
 del tcp_client.exe
 %CC% tcp_client.cpp -o tcp_client.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 tcp_client.exe
 del tcp_client.exe
 del tcp_client.cpp
@@ -118,9 +137,11 @@ cd chap03
 copy tcp_serve_toupper.c tcp_serve_toupper.cpp
 echo "%CC%"
 %CC% tcp_serve_toupper.c -o tcp_serve_toupper.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM tcp_serve_toupper.exe
 del tcp_serve_toupper.exe
 %CC% tcp_serve_toupper.cpp -o tcp_serve_toupper.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM tcp_serve_toupper.exe
 del tcp_serve_toupper.exe
 del tcp_serve_toupper.cpp
@@ -131,9 +152,11 @@ cd chap03
 copy tcp_serve_chat.c tcp_serve_chat.cpp
 echo "%CC%"
 %CC% tcp_serve_chat.c -o tcp_serve_chat.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM tcp_serve_chat.exe
 del tcp_serve_chat.exe
 %CC% tcp_serve_chat.cpp -o tcp_serve_chat.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM tcp_serve_chat.exe
 del tcp_serve_chat.exe
 del tcp_serve_chat.cpp
@@ -145,9 +168,11 @@ cd chap04
 copy udp_client.c udp_client.cpp
 echo "%CC%"
 %CC% udp_client.c -o udp_client.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM udp_client.exe
 del udp_client.exe
 %CC% udp_client.cpp -o udp_client.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM udp_client.exe
 del udp_client.exe
 del udp_client.cpp
@@ -158,9 +183,11 @@ cd chap04
 copy udp_sendto.c udp_sendto.cpp
 echo "%CC%"
 %CC% udp_sendto.c -o udp_sendto.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM udp_sendto.exe
 del udp_sendto.exe
 %CC% udp_sendto.cpp -o udp_sendto.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM udp_sendto.exe
 del udp_sendto.exe
 del udp_sendto.cpp
@@ -171,9 +198,11 @@ cd chap04
 copy udp_recvfrom.c udp_recvfrom.cpp
 echo "%CC%"
 %CC% udp_recvfrom.c -o udp_recvfrom.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM udp_recvfrom.exe
 del udp_recvfrom.exe
 %CC% udp_recvfrom.cpp -o udp_recvfrom.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM udp_recvfrom.exe
 del udp_recvfrom.exe
 del udp_recvfrom.cpp
@@ -184,9 +213,11 @@ cd chap04
 copy udp_serve_toupper.c udp_serve_toupper.cpp
 echo "%CC%"
 %CC% udp_serve_toupper.c -o udp_serve_toupper.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM udp_serve_toupper.exe
 del udp_serve_toupper.exe
 %CC% udp_serve_toupper.cpp -o udp_serve_toupper.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM udp_serve_toupper.exe
 del udp_serve_toupper.exe
 del udp_serve_toupper.cpp
@@ -197,9 +228,11 @@ cd chap04
 copy udp_serve_toupper_simple.c udp_serve_toupper_simple.cpp
 echo "%CC%"
 %CC% udp_serve_toupper_simple.c -o udp_serve_toupper_simple.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM udp_serve_toupper_simple.exe
 del udp_serve_toupper_simple.exe
 %CC% udp_serve_toupper_simple.cpp -o udp_serve_toupper_simple.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM udp_serve_toupper_simple.exe
 del udp_serve_toupper_simple.exe
 del udp_serve_toupper_simple.cpp
@@ -211,9 +244,11 @@ cd chap05
 copy lookup.c lookup.cpp
 echo "%CC%"
 %CC% lookup.c -o lookup.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 lookup.exe example.com
 del lookup.exe
 %CC% lookup.cpp -o lookup.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 lookup.exe example.com
 del lookup.exe
 del lookup.cpp
@@ -224,9 +259,11 @@ cd chap05
 copy dns_query.c dns_query.cpp
 echo "%CC%"
 %CC% dns_query.c -o dns_query.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 dns_query.exe example.com a
 del dns_query.exe
 %CC% dns_query.cpp -o dns_query.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 dns_query.exe example.com a
 del dns_query.exe
 del dns_query.cpp
@@ -238,9 +275,11 @@ cd chap06
 copy web_get.c web_get.cpp
 echo "%CC%"
 %CC% web_get.c -o web_get.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 web_get.exe example.com a
 del web_get.exe
 %CC% web_get.cpp -o web_get.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 web_get.exe example.com
 del web_get.exe
 del web_get.cpp
@@ -251,9 +290,11 @@ cd chap07
 copy web_server.c web_server.cpp
 echo "%CC%"
 %CC% web_server.c -o web_server.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM web_server.exe
 del web_server.exe
 %CC% web_server.cpp -o web_server.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM web_server.exe
 del web_server.exe
 del web_server.cpp
@@ -264,9 +305,11 @@ cd chap07
 copy web_server2.c web_server2.cpp
 echo "%CC%"
 %CC% web_server2.c -o web_server2.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM web_server2.exe
 del web_server2.exe
 %CC% web_server2.cpp -o web_server2.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM web_server2.exe
 del web_server2.exe
 del web_server2.cpp
@@ -277,9 +320,11 @@ cd chap08
 copy smtp_send.c smtp_send.cpp
 echo "%CC%"
 %CC% smtp_send.c -o smtp_send.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM smtp_send.exe
 del smtp_send.exe
 %CC% smtp_send.cpp -o smtp_send.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM smtp_send.exe
 del smtp_send.exe
 del smtp_send.cpp
@@ -293,10 +338,12 @@ cd chap13
 copy connect_timeout.c connect_timeout.cpp
 echo "%CC%"
 %CC% connect_timeout.c -o connect_timeout.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 connect_timeout.exe example.com 80
 connect_timeout.exe example.com 81
 del connect_timeout.exe
 %CC% connect_timeout.cpp -o connect_timeout.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 connect_timeout.exe example.com 80
 connect_timeout.exe example.com 81
 del connect_timeout.exe
@@ -308,10 +355,12 @@ cd chap13
 copy connect_blocking.c connect_blocking.cpp
 echo "%CC%"
 %CC% connect_blocking.c -o connect_blocking.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 connect_blocking.exe example.com 80
 connect_blocking.exe example.com 81
 del connect_blocking.exe
 %CC% connect_blocking.cpp -o connect_blocking.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 connect_blocking.exe example.com 80
 connect_blocking.exe example.com 81
 del connect_blocking.exe
@@ -323,9 +372,11 @@ cd chap13
 copy server_reuse.c server_reuse.cpp
 echo "%CC%"
 %CC% server_reuse.c -o server_reuse.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM server_reuse.exe
 del server_reuse.exe
 %CC% server_reuse.cpp -o server_reuse.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM server_reuse.exe
 del server_reuse.exe
 del server_reuse.cpp
@@ -336,9 +387,11 @@ cd chap13
 copy server_noreuse.c server_noreuse.cpp
 echo "%CC%"
 %CC% server_noreuse.c -o server_noreuse.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM server_noreuse.exe
 del server_noreuse.exe
 %CC% server_noreuse.cpp -o server_noreuse.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM server_noreuse.exe
 del server_noreuse.exe
 del server_noreuse.cpp
@@ -350,9 +403,11 @@ cd chap13
 copy server_crash.c server_crash.cpp
 echo "%CC%"
 %CC% server_crash.c -o server_crash.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM server_crash.exe
 del server_crash.exe
 %CC% server_crash.cpp -o server_crash.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM server_crash.exe
 del server_crash.exe
 del server_crash.cpp
@@ -364,9 +419,11 @@ cd chap13
 copy error_text.c error_text.cpp
 echo "%CC%"
 %CC% error_text.c -o error_text.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 error_text.exe
 del error_text.exe
 %CC% error_text.cpp -o error_text.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 error_text.exe
 del error_text.exe
 del error_text.cpp
@@ -378,9 +435,11 @@ cd chap13
 copy server_ignore.c server_ignore.cpp
 echo "%CC%"
 %CC% server_ignore.c -o server_ignore.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM server_ignore.exe
 del server_ignore.exe
 %CC% server_ignore.cpp -o server_ignore.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM server_ignore.exe
 del server_ignore.exe
 del server_ignore.cpp
@@ -392,9 +451,11 @@ cd chap13
 copy big_send.c big_send.cpp
 echo "%CC%"
 %CC% big_send.c -o big_send.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM big_send.exe
 del big_send.exe
 %CC% big_send.cpp -o big_send.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM big_send.exe
 del big_send.exe
 del big_send.cpp
@@ -406,10 +467,15 @@ cd chap13
 copy setsize.c setsize.cpp
 echo "%CC%"
 %CC% setsize.c -o setsize.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM setsize.exe
 del setsize.exe
 %CC% setsize.cpp -o setsize.exe %CEXTRA%
+if errorlevel 1 set BUILD_FAILED=1
 REM setsize.exe
 del setsize.exe
 del setsize.cpp
 cd ..
+
+if not "%BUILD_FAILED%"=="0" echo One or more examples failed to compile.
+exit /b %BUILD_FAILED%
